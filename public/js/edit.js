baf9289debece053b53e284376d67778ae0a7dce
ArumMelton <103224098+ArumMelton@users.noreply.github.com>
2022-09-16 18:36:21 -0400

    fixed some routing and styling

--- public/js/edit.js
+++ public/js/edit.js
@@ -1,36 +1,29 @@
-const postId = document.querySelector('input[name="post-id"]').value;
-
-const editFormHandler = async function(event) {
+async function editFormHandler(event) {
   event.preventDefault();
 
   const title = document.querySelector('input[name="post-title"]').value;
-  const body = document.querySelector('textarea[name="post-body"]').value;
+  const post_text = document.querySelector('textarea[name="post-text"]').value;
+  const id = window.location.toString().split("/")[
+    window.location.toString().split("/").length - 1
+  ];
 
-  await fetch(`/api/post/${postId}`, {
-    method: 'PUT',
+  const response = await fetch(`/api/posts/${id}`, {
+    method: "PUT",
     body: JSON.stringify({
       title,
-      body
+      post_text,
     }),
     headers: {
-      'Content-Type': 'application/json'
-    }
+      "Content-Type": "application/json",
+    },
   });
-
-  document.location.replace('/dashboard');
-};
-
-const deleteClickHandler = async function() {
-  await fetch(`/api/post/${postId}`, {
-    method: 'DELETE'
-  });
-
-  document.location.replace('/dashboard');
-};
+  if (response.ok) {
+    document.location.replace("/dashboard");
+  } else {
+    alert(response.statusText);
+  }
+}
 
 document
-  .querySelector('#edit-post-form')
-  .addEventListener('submit', editFormHandler);
-document
-  .querySelector('#delete-btn')
-  .addEventListener('click', deleteClickHandler);
+  .querySelector("#edit-post-form")
+  .addEventListener("submit", editFormHandler);
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,24 +1,32 @@
-const signupFormHandler = async function(event) {
+async function signupFormHandler(event) {
   event.preventDefault();
 
-  const usernameEl = document.querySelector('#username-input-signup');
-  const passwordEl = document.querySelector('#password-input-signup');
+  const username = document.querySelector("#username-signup").value;
+  const password = document.querySelector("#password-signup").value;
+  const email = document.querySelector("#email-signup").value;
 
-  const response = await fetch('/api/user', {
-    method: 'POST',
-    body: JSON.stringify({
-      username: usernameEl.value,
-      password: passwordEl.value,
-    }),
-    headers: { 'Content-Type': 'application/json' },
-  });
+  if (username && email && password) {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        password,
+        email,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
 
-  if (response.ok) {
-    document.location.replace('/dashboard');
-  } else {
-    alert('Failed to sign up');
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert(response.statusText);
+    }
   }
-};
+}
+
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
 
 document
   .querySelector('#signup-form')
