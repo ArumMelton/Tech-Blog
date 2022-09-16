1ed76228f2c0e1317b85f929a894da23b31d537f
ArumMelton <103224098+ArumMelton@users.noreply.github.com>
2022-09-16 19:29:50 -0400

    functionality fixes. css is good

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -6,21 +6,25 @@ const userData = [
     id: 1,
     username: "Melt",
     email: "melt@email.com",
+    password: "",
   },
   {
     id: 3,
     username: "John",
     email: "john@email.com",
+    password: "",
   },
   {
     id: 4,
     username: "Christian",
     email: "christian@email.com",
+    password: "",
   },
   {
     id: 5,
     username: "Luis",
     email: "luis@email.com",
+    password: "",
   },
 ];
 
